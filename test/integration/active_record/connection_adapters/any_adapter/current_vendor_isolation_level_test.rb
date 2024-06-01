23cbfefb20e209fa07feb40c165067a8a9f9738a
Iago Pimenta <iagopimenta.s@gmail.com>
2024-06-01 17:22:03 -0300

    fix: rubocop offenses

--- test/integration/active_record/connection_adapters/any_adapter/current_vendor_isolation_level_test.rb
+++ test/integration/active_record/connection_adapters/any_adapter/current_vendor_isolation_level_test.rb
@@ -1,33 +1,25 @@
-# -*- encoding : utf-8 -*-
+# frozen_string_literal: true
 
 require 'test_helper'
 
 class ActiveRecordTest < Minitest::Test
-
   class ConnectionAdaptersTest < Minitest::Test
-
     class AnyAdapterTest < Minitest::Test
-
       class CurrentVendorIsolationLevelTest < Minitest::Test
-
         def test_returns_correct_default_vendor_isolation_level
-          if defined?( ActiveRecord::ConnectionAdapters::Mysql2Adapter )
-            assert_equal( 'REPEATABLE READ', ActiveRecord::Base.connection.current_vendor_isolation_level )
+          if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+            assert_equal('REPEATABLE READ', ActiveRecord::Base.connection.current_vendor_isolation_level)
           end
 
-          if defined?( ActiveRecord::ConnectionAdapters::PostgreSQLAdapter )
-            assert_equal( 'READ COMMITTED', ActiveRecord::Base.connection.current_vendor_isolation_level )
+          if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+            assert_equal('READ COMMITTED', ActiveRecord::Base.connection.current_vendor_isolation_level)
           end
 
-          if defined?( ActiveRecord::ConnectionAdapters::SQLite3Adapter )
-            assert_equal( 'read_uncommitted = 0', ActiveRecord::Base.connection.current_vendor_isolation_level )
-          end
+          return unless defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+
+          assert_equal('read_uncommitted = 0', ActiveRecord::Base.connection.current_vendor_isolation_level)
         end
-
       end
-
     end
-
   end
-
 end
--- test/library_setup.rb
+++ test/library_setup.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 # Prepares application to be tested (requires files, connects to db, resets schema and data, applies patches, etc.)
 
 # Initialize database
 require 'db/all'
 
 case ENV['db']
-  when 'mysql2'
-    TransactionIsolation::Test::Db.connect_to_mysql2
-  when 'postgresql'
-    TransactionIsolation::Test::Db.connect_to_postgresql
-  when 'sqlite3'
-    TransactionIsolation::Test::Db.connect_to_sqlite3
-  else
-    TransactionIsolation::Test::Db.connect_to_sqlite3
+when 'mysql2'
+  TransactionIsolation::Test::Db.connect_to_mysql2
+when 'postgresql'
+  TransactionIsolation::Test::Db.connect_to_postgresql
+when 'sqlite3'
+  TransactionIsolation::Test::Db.connect_to_sqlite3
+else
+  TransactionIsolation::Test::Db.connect_to_sqlite3
 end
 
 TransactionIsolation::Test::Migrations.run!
@@ -20,6 +22,6 @@ TransactionIsolation::Test::Migrations.run!
 require 'transaction_isolation'
 
 require 'logger'
-ActiveRecord::Base.logger = Logger.new( File.expand_path( "#{File.dirname( __FILE__ )}/log/test.log" ) )
+ActiveRecord::Base.logger = Logger.new(File.expand_path("#{File.dirname(__FILE__)}/log/test.log"))
 
 TransactionIsolation.apply_activerecord_patch
